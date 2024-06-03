364311adbef7d54305a1f3af040bc39ea1f92aa7
Pascual Vila <46532433+pascual143@users.noreply.github.com>
2024-06-03 11:00:38 +0200

    Create Exercises.js

---
+++ Exercises.js
@@ -0,0 +1,18 @@
+// Exercises
+
+// 1. Sort a dictionary by value:
+// Write a function that sorts a dictionary (object) by its values from highest to lowest. The dictionary can contain any data type (numbers, strings, etc.).
+const dictionary = {
+  "a": 10,
+  "b": 2,
+  "c": 5,
+  "d": 1
+}
+
+function sortDictionaryByValue(dictionary) {
+   // Impliment the function here
+}
+
+console.log(sortDictionaryByValue(dictionary)) // Expected result: (d: 1, b:2, c: 5, a: 10)
+
+
